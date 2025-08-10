a9471f20f04837ceec8dc2e1f2266539b9def800
sync-1024 <versioncontrol@synctecho.com>
2025-08-10 17:51:31 +0500

    real backend link is attached

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
-export const Base_URL = 'http://localhost:5049/api';
+// export const Base_URL = 'http://localhost:5049/api';
+export const Base_URL = 'https://trainmanagement-production.up.railway.app/api';

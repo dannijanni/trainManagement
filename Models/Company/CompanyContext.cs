100a4bcd27c4c460dd2b23522801808dc1536049
sync-1024 <versioncontrol@synctecho.com>
2025-08-11 10:39:46 +0500

    credientials secure.

--- Models/Company/CompanyContext.cs
+++ Models/Company/CompanyContext.cs
@@ -61,7 +61,7 @@ public partial class CompanyContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=train_management;User Id=Decent_Customers;Password=SyncSoftDecent9658;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=${DB_SERVER}; database=train_management; User ID=${DB_USER}; Password=${DB_PASS}; TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {

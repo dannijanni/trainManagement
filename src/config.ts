05135365d8a7cd332d628a6012db1748bc4c4d90
sync-1024 <versioncontrol@synctecho.com>
2025-08-31 13:21:39 +0500

    Local to Server Shifted

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-//export const Base_URL = import.meta.env.VITE_API_URL;
-export const Base_URL = "http://localhost:5049/api";
+export const Base_URL = import.meta.env.VITE_API_URL;
+// export const Base_URL = "http://localhost:5049/api";
